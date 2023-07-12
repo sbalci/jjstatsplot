791067774b478b92bd10ab75d57576fd03c4fd3b
sbalci <drserdarbalci@gmail.com>
2023-07-13 00:22:32 +0300

    UP

---
+++ R/jjstatsplot-package.R
@@ -0,0 +1,16 @@
+#' \code{jjstatsplot}
+#'
+#' @title jjstatsplot: Wrapper for ggstatsplot
+#'
+#' @description `jjstatsplot` A wrapper for ggstatsplot: jjstatsplot help
+#' researchers to generate plots in jamovi based on ggstatsplot package.
+#'
+#'
+#'
+#' For more documentation, see the
+#' \href{https://sbalci.github.io/ClinicoPathJamoviModule/}{Website}.
+#'
+#' @docType package
+#' @aliases jjstatsplot jjstatsplot-package
+#' @name jjstatsplot-package
+"_PACKAGE"
