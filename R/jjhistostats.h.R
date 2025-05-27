d35608843e0270f03980b534a43c9e26eb42e5ce
sbalci <drserdarbalci@gmail.com>
2025-05-27 12:33:40 +0300

    update for jamovi 2.7

--- R/jjhistostats.h.R
+++ R/jjhistostats.h.R
@@ -141,7 +141,7 @@ jjhistostatsBase <- if (requireNamespace("jmvcore", quietly=TRUE)) R6::R6Class(
             super$initialize(
                 package = "jjstatsplot",
                 name = "jjhistostats",
-                version = c(1,0,0),
+                version = c(0,0,3),
                 options = options,
                 results = jjhistostatsResults$new(options=options),
                 data = data,
